041325334b2eb88f383b7d28e664e7cea908f598
fitumi0 <chervyakov2002anton@mail.ru>
2023-04-12 12:10:09 +0300

    added footer page with custom actions

--- ContextMenuEditorForWindows/MainWindow.xaml.cs
+++ ContextMenuEditorForWindows/MainWindow.xaml.cs
@@ -2,31 +2,12 @@
 // Licensed under the MIT License.
 
 using ContextMenuEditorForWindows.Views;
-using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security.AccessControl;
-using System.Security.Principal;
-using System.Xml.Linq;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.System;
-using Windows.UI.ApplicationSettings;
-using Windows.UI.Core;
-using static System.Net.WebRequestMethods;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
