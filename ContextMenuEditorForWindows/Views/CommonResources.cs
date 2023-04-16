d83e45d390eb79851a1316f4c1aff7467f922a8c
fitumi0 <chervyakov2002anton@mail.ru>
2023-04-16 16:00:48 +0300

    common resources (constants, readonly arrays or keys etc.)

---
+++ ContextMenuEditorForWindows/Views/CommonResources.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ContextMenuEditorForWindows.Views;
+internal class CommonResources
+{
+    public static readonly string[] hiddenKeys = {
+            "removeproperties",
+            "explore",
+            "open",
+            "opennewprocess",
+            "opennewwindow",
+            "find",
+            "updateencryptionsettings",
+            "updateencryptionsettingswork",
+            "cmd",//отключаемо
+            "powershell",//отключаемо
+            "wsl"//отключаемо
+        };
+
+    public static readonly string regPattern = @"^\{[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}\}$";
+
+    public static RegistryKey CLSID = Registry.ClassesRoot.OpenSubKey("CLSID", true);
+
+
+}
