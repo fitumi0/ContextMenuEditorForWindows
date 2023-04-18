869329b8196e85c68810eb6990f81e1307fff75a
fitumi0 <chervyakov2002anton@mail.ru>
2023-04-18 23:15:26 +0300

    added hardcoded location to tools and add\remove action Pack to registry

--- ContextMenuEditorForWindows/Views/CustomActions.xaml.cs
+++ ContextMenuEditorForWindows/Views/CustomActions.xaml.cs
@@ -23,7 +23,7 @@ namespace ContextMenuEditorForWindows.Views
     /// </summary>
     public sealed partial class CustomActions : Page
     {
-
+        RegistryKey packItems = Registry.ClassesRoot.OpenSubKey("Directory", true).OpenSubKey("Background", true).OpenSubKey("shell", true);
         public CustomActions()
         {
             this.InitializeComponent();
@@ -47,31 +47,72 @@ namespace ContextMenuEditorForWindows.Views
         private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
         {
         }
-        
+
+        private void Pass(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch ts = (sender as ToggleSwitch);
+            //((ts.Parent as StackPanel).Children[1] as TextBlock).Text = Properties.Settings.Default.CopyPath;
+        }
         private async void PackToggle(object sender, RoutedEventArgs e)
         {
-            var dialog = new ContentDialog();
-            dialog.XamlRoot = this.XamlRoot;
-            dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
-            dialog.Title = "Новый элемент Контекстного меню";
-            dialog.PrimaryButtonText = "Создать";
-            dialog.CloseButtonText = "Отмена";
-            //dialog.PrimaryButtonClick = AddButton_Click;
-            dialog.DefaultButton = ContentDialogButton.Primary;
-            await dialog.ShowAsync();
+            string keyName = "PackWithContext";
+            ToggleSwitch ts = (sender as ToggleSwitch);
+            RegistryKey _rk = packItems.OpenSubKey(keyName, true);
+            if (ts != null)
+            {
+                
+                if (_rk == null)
+                {
+                    RegistryKey packKey = packItems.CreateSubKey(keyName, true);
+                    packKey.CreateSubKey("command", true).SetValue("", 
+                        // todo : copy tools to some folder. default value or from settings
+                        "\"C:\\Applications\\AppData\\Projects\\CSharp\\ContextMenuEditorForWindows\\ContextMenuTools\\bin\\Debug\\net6.0\\ContextMenuTools.exe\" /PackFiles \"%V\"");
+                }
+                else if (!ts.IsOn)
+                {
+                    packItems.DeleteSubKeyTree(keyName);
+                }
+
+            }
+            
+            //var dialog = new ContentDialog();
+            //dialog.XamlRoot = this.XamlRoot;
+            //dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+            //dialog.Title = "Новый элемент Контекстного меню";
+            //dialog.PrimaryButtonText = "Создать";
+            //dialog.CloseButtonText = "Отмена";
+            ////dialog.PrimaryButtonClick = AddButton_Click;
+            //dialog.DefaultButton = ContentDialogButton.Primary;
+            //await dialog.ShowAsync();
         }
 
         private void ListOfCustomActions_Loaded(object sender, RoutedEventArgs e)
         {
             ListViewCustomActionTemplate lv = new ListViewCustomActionTemplate
                 (
-                    "",
                     "Pack Items To Folder",
+                    packItems.GetSubKeyNames().Contains("PackWithContext") ? packItems.OpenSubKey("PackWithContext", true).GetValue("LegacyDisable") == null : false, // проверка на то, есть ли в реестре уже ключ. если есть (он может быть выключен), проверяем активен ли
                     false,
+                    "Directory Background",
                     true,
                     PackToggle
                 );
             ListOfCustomActions.Items.Add(lv);
+            ListViewCustomActionTemplate l1v = new ListViewCustomActionTemplate
+                (
+                    "Veryveryveryveryveryveryveryvery long name",
+                    false,
+                    true,
+                    "Drive",
+                    true,
+                    Pass
+                );
+            ListOfCustomActions.Items.Add(l1v);
+        }
+
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            //(((sender as MenuFlyoutItem).Parent) as DropDownButton).Content = (sender as MenuFlyoutItem).Text;
         }
 
         private async void DeleteButton_Click(object sender, RoutedEventArgs e)
