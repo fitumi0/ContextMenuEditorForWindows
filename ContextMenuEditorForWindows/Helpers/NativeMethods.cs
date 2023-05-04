e35eefbd38cd90c488eb7aac6dee728c1ee17323
fitumi0 <chervyakov2002anton@mail.ru>
2023-05-04 23:24:46 +0300

    ?

---
+++ ContextMenuEditorForWindows/Helpers/NativeMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ContextMenuEditorForWindows.Helpers;
+static class NativeMethods
+{
+    const int SW_HIDE = 0;
+    const int SW_SHOW = 5;
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr LoadLibrary(string dllToLoad);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+    [DllImport("kernel32.dll")]
+    public static extern bool FreeLibrary(IntPtr hModule);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int LoadString(IntPtr hInstance, uint uID, StringBuilder lpBuffer, int nBufferMax);
+
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+}
