97733609e9fd812fedb926fd5d7b235bb704f369
fitumi0 <chervyakov2002anton@mail.ru>
2023-05-04 23:27:58 +0300

    added json settings file

---
+++ ContextMenuEditorForWindows/Helpers/SettingsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace ContextMenuEditorForWindows.Helpers;
+class Settings
+{
+    public string Path { get; set; }
+    //public string SomeItem { get; set; }
+    public void Save(string filePath)
+    {
+        var json = JsonConvert.SerializeObject(this, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(filePath, json);
+    }
+
+    public static Settings Load(string filePath)
+    {
+        var json = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<Settings>(json);
+    }
+}
