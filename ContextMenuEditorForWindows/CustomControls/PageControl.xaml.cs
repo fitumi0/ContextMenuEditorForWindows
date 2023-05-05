e70fe876c96cd0ebcf67a70110c3579d879cf2a7
fitumi0 <chervyakov2002anton@mail.ru>
2023-05-05 19:27:54 +0300

    attempt to attach file picker, win11 crash, need fix

--- ContextMenuEditorForWindows/CustomControls/PageControl.xaml.cs
+++ ContextMenuEditorForWindows/CustomControls/PageControl.xaml.cs
@@ -2,19 +2,11 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using ContextMenuEditorForWindows.Helpers;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -50,5 +42,31 @@ namespace ContextMenuEditorForWindows.CustomControls
             }
         }
 
+        private void LocationCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var dialog = Root.Parent as ContentDialog;
+            //dialog.PrimaryButtonClick;
+            // change Primary Button action
+        }
+
+        private async void PickComandFile(object sender, RoutedEventArgs e)
+        {
+            // TODO: FIX
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            // Retrieve the window handle (HWND) of the current WinUI 3 window.
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+
+            // Initialize the file picker with the window handle (HWND).
+            WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+            // Set options for your file picker
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add("*");
+
+            // Open the picker for the user to pick a file
+            var file = await openPicker.PickSingleFileAsync();
+
+        }
     }
 }
