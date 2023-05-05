0216dfd328f79e64071641139fb5a5f09ea3582f
fitumi0 <chervyakov2002anton@mail.ru>
2023-05-05 19:29:12 +0300

    ?

--- ContextMenuEditorForWindows/App.xaml.cs
+++ ContextMenuEditorForWindows/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using ContextMenuEditorForWindows.Helpers;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
