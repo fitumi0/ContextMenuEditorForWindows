5deb8e29a4d9a00b253f6b0a615a19f5cf0f0fde
fitumi0 <chervyakov2002anton@mail.ru>
2023-05-06 23:22:15 +0300

    added OpenFileDialog method and some filetypes

--- ContextMenuEditorForWindows/Helpers/NativeMethods.cs
+++ ContextMenuEditorForWindows/Helpers/NativeMethods.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using Microsoft.UI.Xaml.Controls;
 
 namespace ContextMenuEditorForWindows.Helpers;
 static class NativeMethods
@@ -25,4 +27,65 @@ static class NativeMethods
 
     [DllImport("user32.dll")]
     static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [DllImport("comdlg32.dll", CharSet = CharSet.Auto)]
+    public static extern bool GetOpenFileName(ref OPENFILENAME lpofn);
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct OPENFILENAME
+    {
+        public int lStructSize;
+        public IntPtr hwndOwner;
+        public IntPtr hInstance;
+        public string lpstrFilter;
+        public string lpstrCustomFilter;
+        public int nMaxCustFilter;
+        public int nFilterIndex;
+        public string lpstrFile;
+        public int nMaxFile;
+        public string lpstrFileTitle;
+        public int nMaxFileTitle;
+        public string lpstrInitialDir;
+        public string lpstrTitle;
+        public int Flags;
+        public short nFileOffset;
+        public short nFileExtension;
+        public string lpstrDefExt;
+        public IntPtr lCustData;
+        public IntPtr lpfnHook;
+        public string lpTemplateName;
+        public IntPtr pvReserved;
+        public int dwReserved;
+        public int FlagsEx;
+    }
+
+    private static Dictionary<string, string> FILETYPES = new ()
+    {
+        { "txt", "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0" },
+        { "*", "All Files (*.*)\0*.*\0"},
+        { "ico", "Icons (*.ico)\0*.ico\0"},
+        { "exe", "Executable Files (*.exe)\0*.exe\0"},
+
+    };
+
+    public static string OpenFileDialog(string fileType)
+    {
+        var ofn = new OPENFILENAME();
+        ofn.lStructSize = Marshal.SizeOf(ofn);
+        ofn.lpstrFilter = FILETYPES[fileType];
+        ofn.lpstrFile = new string('\0', 260);
+        ofn.nMaxFile = ofn.lpstrFile.Length;
+        ofn.lpstrFileTitle = new string('\0', 260);
+        ofn.nMaxFileTitle = ofn.lpstrFileTitle.Length;
+        ofn.lpstrInitialDir = "C:\\";
+        ofn.lpstrTitle = "Open File";
+        ofn.Flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;
+
+        if (GetOpenFileName(ref ofn))
+        {
+            // The user selected a file, do something with it
+            return ofn.lpstrFile;
+        }
+        return "";
+    }
 }
