d72aae1689c5526df84ea99b250101c28b01e505
fitumi0 <chervyakov2002anton@mail.ru>
2023-05-11 00:36:15 +0300

    added custom action class

---
+++ ContextMenuEditorForWindows/Templates/CustomAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+
+namespace ContextMenuEditorForWindows.Templates;
+public class CustomAction
+{
+    public string Title { get; set; }
+    public string Command { get; set; }
+    public string Location { get; set; }
+
+    public CustomAction(string title, string command, string location)
+    {
+        Title = title;
+        Command = command;
+        Location = location;
+    }
+}
