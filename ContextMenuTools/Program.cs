cdaeccdb27dffeffec0bf67bf4caace9876c5ebe
fitumi0 <chervyakov2002anton@mail.ru>
2023-05-10 19:53:51 +0300

    removed unused imports

--- ContextMenuTools/Program.cs
+++ ContextMenuTools/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Runtime.InteropServices;
-using ContextMenuTools;
-
-
+﻿using ContextMenuTools;
 
 class Program
 {
