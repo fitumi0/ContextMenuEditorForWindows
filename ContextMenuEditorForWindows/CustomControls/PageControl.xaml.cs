da5c32edd668c058e6064a2b5f804184ca4a6c76
fitumi0 <chervyakov2002anton@mail.ru>
2023-05-19 19:56:39 +0300

    added validation for title and command (temporarily, in the future I think to add parameters such as %V)

--- ContextMenuEditorForWindows/CustomControls/PageControl.xaml.cs
+++ ContextMenuEditorForWindows/CustomControls/PageControl.xaml.cs
@@ -1,8 +1,14 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using System;
+using System.IO;
+using System.Linq;
+using ContextMenuEditorForWindows.Helpers;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI;
 using static ContextMenuEditorForWindows.Helpers.NativeMethods;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -12,6 +18,34 @@ namespace ContextMenuEditorForWindows.CustomControls
 {
     public sealed partial class PageControl : UserControl
     {
+        private static readonly char[] InvalidFileNameChars = Path.GetInvalidFileNameChars();
+        private static readonly string[] ReservedNames = new[]
+        {
+        "CON", "PRN", "AUX", "NUL",
+        "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+        "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"
+    };
+
+        private bool IsValidFileName(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return false;
+            }
+
+            if (InvalidFileNameChars.Any(c => fileName.Contains(c)))
+            {
+                return false;
+            }
+
+            string nameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            if (ReservedNames.Contains(nameWithoutExtension, StringComparer.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            return true;
+        }
         public PageControl()
         {
             this.InitializeComponent();
@@ -21,11 +55,26 @@ namespace ContextMenuEditorForWindows.CustomControls
         {
             var dialog = Root.Parent as ContentDialog;
             // Validate the TextBox content
-            if (string.IsNullOrEmpty(TitleBox.Text) || string.IsNullOrEmpty(CommandBox.Text))
+            AppSettings settings = Settings.LoadFromFile<AppSettings>();
+            if (!File.Exists(CommandBox.Text))
             {
                 dialog.IsPrimaryButtonEnabled = false;
+                CommandBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x86, 0x1B, 0x2D));
             }
             else
+            {
+                CommandBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x32, 0x32, 0x32));
+            }
+            if (!IsValidFileName(TitleBox.Text))
+            {
+                dialog.IsPrimaryButtonEnabled = false;
+                TitleBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x86, 0x1B, 0x2D));
+            }
+            else
+            {
+                TitleBox.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x32, 0x32, 0x32));
+            }
+            if (File.Exists(CommandBox.Text) && IsValidFileName(TitleBox.Text))
             {
                 dialog.IsPrimaryButtonEnabled = true;
             }
